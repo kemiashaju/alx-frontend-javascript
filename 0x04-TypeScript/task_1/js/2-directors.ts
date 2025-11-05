d80fd0228958274c53a60253a7307c822bd2fd20
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-05 14:50:08 +0100

    fix

--- 0x04-TypeScript/task_1/js/2-directors.ts
+++ 0x04-TypeScript/task_1/js/2-directors.ts
@@ -1,9 +1,3 @@
-import { Teacher } from "./main";
-
-export interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -11,5 +5,12 @@ const director1: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
-
 console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
