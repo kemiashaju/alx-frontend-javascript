0cff2b4158b96399381602bcf89b0979870918fc
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-05 13:57:19 +0100

    directors

---
+++ 0x04-TypeScript/task_1/js/2-directors.ts
@@ -0,0 +1,15 @@
+import { Teacher } from "./main";
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
